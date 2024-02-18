b0cf767b4318383f4f7d158ddeb5d2b66afecafc
Joshua Kaluba <joshkaluba@gmail.com>
2024-02-18 17:07:19 -0500

    Added ability to get a monitors notes, locations, and details from endpoint

---
+++ api/src/NeverAlone.Business/DTO/MonitorNoteLocationDto.cs
@@ -0,0 +1,10 @@
+namespace NeverAlone.Business.DTO;
+
+public class MonitorNoteLocationDto
+{
+    public UserMonitorDto Monitor { get; set; }
+    
+    public IEnumerable<NoteDto>? Notes { get; set; }
+    
+    public IEnumerable<MonitorLocationDto>? Locations { get; set; }
+}
---
+++ api/src/NeverAlone.Web/Controllers/MonitorsController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NeverAlone.Business.DTO;
+using NeverAlone.Business.Services.MonitoredLocations;
+using NeverAlone.Business.Services.Monitors;
+using NeverAlone.Business.Services.Notes;
+
+namespace NeverAlone.Web.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class MonitorsController : ControllerBase
+{
+    private readonly ILogger<MonitorsController> _logger;
+    private readonly IMonitorLocationService _monitorLocationService;
+    private readonly IMonitorService _monitorService;
+    private readonly INoteService _noteService;
+
+    public MonitorsController(IMonitorService monitorService,
+        INoteService noteService,
+        IMonitorLocationService monitorLocationService,
+        ILogger<MonitorsController> logger
+    )
+    {
+        _monitorService = monitorService;
+        _logger = logger;
+        _noteService = noteService;
+        _monitorLocationService = monitorLocationService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetMonitor(Guid id)
+    {
+        var monitor = await _monitorService.GetMonitorByIdAsync(id);
+        if (monitor == null) return NotFound("Unable to find a valid monitor");
+
+        var notes = await _noteService.GetNoteByMonitorAsync(id);
+        var locations = await _monitorLocationService.GetMonitorLocationByMonitorAsync(id);
+
+
+        var dto = new MonitorNoteLocationDto
+        {
+            Monitor = monitor,
+            Notes = notes,
+            Locations = locations
+        };
+
+        return Ok(dto);
+    }
+}
