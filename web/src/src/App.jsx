9800baeac1fb8b6be892b6aa7f879101b4e1c7ca
Joshua Kaluba <joshkaluba@gmail.com>
2024-02-18 21:01:32 -0500

    Switch to hashrouter

--- web/src/src/App.jsx
+++ web/src/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import NoPage from "./pages/NoPage";
@@ -7,14 +7,14 @@ import NoPage from "./pages/NoPage";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path=":guid" element={<HomePage />} />
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
