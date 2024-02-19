0146031142832c8b15163b10e663871a1f02ff9c
Joshua Kaluba <joshkaluba@gmail.com>
2024-02-19 03:37:11 -0500

    Update NoteDto.cs

--- api/src/NeverAlone.Business/DTO/NoteDto.cs
+++ api/src/NeverAlone.Business/DTO/NoteDto.cs
@@ -3,6 +3,7 @@ namespace NeverAlone.Business.DTO;
 public class NoteDto
 {
     public Guid? Id { get; set; } = Guid.NewGuid();
+    
     public string? TextContext { get; set; }
     public Guid UserMonitorId { get; set; }
 
